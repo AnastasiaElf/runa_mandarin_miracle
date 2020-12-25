f2545c12c456a08f2b19376794d4bcc87b5a9684
AnastasiaElf <anastasia.elf.17@gmail.com>
2020-12-25 22:41:39 +0100

    Add scene background

--- scripts.js
+++ scripts.js
@@ -11,6 +11,35 @@ startButton.addEventListener("click", (e) => {
   startGame();
 });
 
+const SCENE_HEIGHT = 400;
+const CHARACTER_HEIGHT = 70;
+const STEP = CHARACTER_HEIGHT;
+const INIT_Y = 400 / 2 - CHARACTER_HEIGHT / 2;
+const MAX_Y = INIT_Y + STEP * 2;
+const MIN_Y = INIT_Y - STEP * 2;
+
 function startGame() {
+  character.style.top = `${INIT_Y}px`;
   character.classList.remove("hidden");
+
+  window.onkeydown = (e) => {
+    const key = e.code;
+    const top = parseInt(character.style.top);
+
+    switch (key) {
+      case "ArrowUp":
+        if (top > MIN_Y) {
+          character.style.top = `${top - STEP}px`;
+        }
+        break;
+      case "ArrowDown":
+        if (top < MAX_Y) {
+          character.style.top = `${top + STEP}px`;
+        }
+        break;
+
+      default:
+        break;
+    }
+  };
 }
